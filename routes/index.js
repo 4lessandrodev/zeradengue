bd6a65e1793a0a735bf94727bee00c9004e7c9e9
Alessandro Menezes <alessandroadm@live.com>
2020-04-28 22:17:26 -0300

    pagina cadastro concluida

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET cadastro page. */
+router.get('/cadastro', function (req, res, next) {
+  res.render('cadastro', { title: 'Express' });
+});
+
 module.exports = router;
