939115eaa05d416b90259cadfe2e4c34d6c51c74
Alessandro Menezes <alessandroadm@live.com>
2020-05-01 16:52:12 -0300

    pagina de denuncia selecionada feito

--- routes/users.js
+++ routes/users.js
@@ -11,5 +11,9 @@ router.get('/', function(req, res, next) {
 router.get('/denuncia', function (req, res, next) {
   res.render('usuario/denuncia');
 });
+/* GET nova denuncia selecionada listing. */
+router.get('/denuncia/:id', function (req, res, next) {
+  res.render('usuario/denuncia-selecionada');
+});
 
 module.exports = router;
