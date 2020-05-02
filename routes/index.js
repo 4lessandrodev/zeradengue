942bcdade790eefd2380c1db26aaa459360b57ff
Alessandro Menezes <alessandroadm@live.com>
2020-05-01 22:37:14 -0300

    refatorando as views com include

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Login' });
 });
 
 /* GET cadastro page. */
 router.get('/cadastro', function (req, res, next) {
-  res.render('cadastro', { title: 'Express' });
+  res.render('cadastro', { title: 'Cadastro' });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,23 +3,23 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('usuario/index');
+  res.render('usuario/index',{title:'Home'});
 });
 
 
 /* GET nova denuncia listing. */
 router.get('/denuncia', function (req, res, next) {
-  res.render('usuario/denuncia');
+  res.render('usuario/denuncia',{title:'Denúncia'});
 });
 
 /* GET denuncia selecionada listing. */
 router.get('/denuncia/:id', function (req, res, next) {
-  res.render('usuario/denuncia-selecionada');
+  res.render('usuario/denuncia-selecionada',{title:'Denúncia Selecionada'});
 });
 
 /* GET perfil do usuário listing. */
 router.get('/perfil', function (req, res, next) {
-  res.render('usuario/usuario');
+  res.render('usuario/usuario',{title:'Perfil'});
 });
 
 module.exports = router;
